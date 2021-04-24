4d4b2d8e730e45dd140371177dd0005b965c8a77
calel <calel@pop-os.localdomain>
2021-04-23 21:31:03 -0600

    initial commit

---
+++ apps/kinini-shop-ui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'kinini-shop-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'kinini-shop-ui';
+}
