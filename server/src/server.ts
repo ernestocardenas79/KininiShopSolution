c3a3bda55854b102a1f6cd7a4462fa08b7cc00be
Ernesto Cardenas <ernesto.cardenas79@gmail.com>
2023-05-29 22:43:10 -0600

    added eslint and prettier

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,28 +1,28 @@
-import { ApolloServer} from 'apollo-server-express';
-import compression from "compression";
-import cors from "cors";
-import express from "express";
-import chalk from "chalk";
+import {ApolloServer} from 'apollo-server-express';
+import compression from 'compression';
+import cors from 'cors';
+import express from 'express';
+import chalk from 'chalk';
 
 import schema from './schema';
 
-async function init(){
+async function init() {
+	const app = express();
 
-    const app = express();
-    
-    app
-    // .use('*',cors)
-        .use(compression());
-    
-     const server = new ApolloServer({
-        schema,
-        introspection: true
-    })  
-    
-    const PORT = process.env.PORT || 2002;
-    await server.start()
-        server.applyMiddleware({app});    
-        app.listen(PORT);
+	app
+	// .use('*',cors)
+		.use(compression());
+
+	const server = new ApolloServer({
+		schema,
+		introspection: true,
+	});
+
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	const PORT = process.env.PORT ?? 2002;
+	await server.start();
+	server.applyMiddleware({app});
+	app.listen(PORT);
 }
 
-init();
+void init();
