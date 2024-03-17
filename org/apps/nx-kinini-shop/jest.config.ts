7bfe1e865c2b9c3547848ecbd20fcb2c4d66f27f
Ernesto Cardenas <ernesto.cardenas79@gmail.com>
2024-03-17 10:20:56 -0700

    starting with nx and graphql

---
+++ org/apps/nx-kinini-shop/jest.config.ts
@@ -0,0 +1,14 @@
+/* eslint-disable */
+export default {
+    displayName: 'nx-kinini-shop',
+    preset: '../../jest.preset.js',
+    testEnvironment: 'node',
+    transform: {
+        '^.+\\.[tj]s$': [
+            'ts-jest',
+            { tsconfig: '<rootDir>/tsconfig.spec.json' },
+        ],
+    },
+    moduleFileExtensions: ['ts', 'js', 'html'],
+    coverageDirectory: '../../coverage/apps/nx-kinini-shop',
+};
